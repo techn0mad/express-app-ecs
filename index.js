e0af0d54e91f05ac9497c22263f721e26aeb008f
Larry Gadallah <larry@gadallah.com>
2024-05-19 21:58:25 -0700

    Changing greeting to check automatic deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World, from AWS ECS!!</h1>')
+  res.send('<h1>Hello World, this is AWS ECS!!</h1>')
 })
 
 const PORT = 8080
